b5975e3fec6456716389e5014be0cc6d2a7c3aee
Ashishansurkar <56146276+Ashishansurkar@users.noreply.github.com>
2020-10-30 14:32:07 +1300

    deploy

--- practice/__init__.py
+++ practice/__init__.py
@@ -1,23 +1,29 @@
 from flask import *
 # from flask_sqlalchemy import SQLAlchemy
+from os import environ
 from sqlalchemy import *
 from sqlalchemy.orm import (scoped_session,sessionmaker,relationship,backref)
 from sqlalchemy.ext.declarative import declarative_base
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
-# db_uri = 'sqlite:///site.db'
-db_uri = environ.get('DATABASE_URL')
 
-engine = create_engine(db_uri,convert_unicode=True)
+# db_uri = 'sqlite:///site.db'
+# DATABASE_URL = 'sqlite:///site.db'
+# SECRET_KEY = 'ashish'
+# if environ:
+DATABASE_URL = environ.get('DATABASE_URL')
+SECRET_KEY = environ.get('SECRET_KEY')
+
+engine = create_engine(DATABASE_URL,convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
